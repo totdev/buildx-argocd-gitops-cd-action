d2d8ca4758e7e4e2cf7932299e10c38ec0e7b48c
rvoliveirafurg <66887480+rvoliveirafurg@users.noreply.github.com>
2021-08-15 21:56:05 -0300

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -73,7 +73,7 @@ echo "yq w -i ${YAML_FILE} ${YAML_FILE_IMAGE_TAG_KEY} ${IMAGE_TAG}"
 
 mkdir -p $HOME/deployment-repo
 
-git clone https://$DEPLOYMENT_REPO_TOKEN@github.com/$DEPLOYMENT_REPO /deployment-repo || exit 1
+git clone https://$DEPLOYMENT_REPO_TOKEN@github.com/$DEPLOYMENT_REPO $HOME/deployment-repo || exit 1
 yq w -i ${YAML_FILE} ${YAML_FILE_IMAGE_TAG_KEY} ${IMAGE_TAG} || exit 1
 
 
