01a7c55d36290fb3c74d8155d64cf639cff769d2
rvoliveirafurg <66887480+rvoliveirafurg@users.noreply.github.com>
2021-08-16 03:31:30 -0300

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -58,7 +58,7 @@ buildx build $ARGS || exit 1
 echo "Cloning deployment repo"
 
 export ENVIRONMENT=${INPUT_ENVIRONMENT}
-export YAML_FILE=./applications/deployments/$APPLICATION/$ENVIRONMENT/${INPUT_YAML_FILE}
+export YAML_FILE=$HOME/deployment-repo/applications/deployments/$APPLICATION/$ENVIRONMENT/${INPUT_YAML_FILE}
 export YAML_FILE_IMAGE_TAG_KEY=${INPUT_YAML_FILE_IMAGE_TAG_KEY}
 
 
@@ -66,8 +66,8 @@ export YAML_FILE_IMAGE_TAG_KEY=${INPUT_YAML_FILE_IMAGE_TAG_KEY}
 #echo "yq w -i ${YAML_FILE} ${YAML_FILE_IMAGE_TAG_KEY} ${IMAGE_TAG}"
 
 
-#mkdir -p /deployment-repo
-git clone https://$DEPLOYMENT_REPO_TOKEN@github.com/$DEPLOYMENT_REPO /deployment-repo || exit 1
+mkdir -p $HOME/deployment-repo
+git clone https://$DEPLOYMENT_REPO_TOKEN@github.com/$DEPLOYMENT_REPO $HOME/deployment-repo || exit 1
 
 
 echo "YAML_FILE: $YAML_FILE"
@@ -78,7 +78,7 @@ echo "IMAGE_TAG: $IMAGE_TAG"
 
 #applications/deployments/n381-api/production/kustomization.yaml
 
-cd /deployment-repo
+cd $HOME/deployment-repo
 yq w -i ${YAML_FILE} ${YAML_FILE_IMAGE_TAG_KEY} ${IMAGE_TAG} || exit 1
 git config --local user.email "actions@github.com"
 git config --local user.name "GitHub Actions"
